365b037577bcc7d4000f42b524ee6e12c564fe7f
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2022-01-12 17:59:12 -0400

    add img node to html

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
 **/
-
+console.log("Hello world!")
 // crear (1) imagen
 // agregar #imagen
 
@@ -11,12 +11,14 @@
 </div> */}
 
 const createImageNode= () => {
+    // create img
     const image = document.createElement("img");
     image.className = "mx-auto";
-    image.width = "320px";
-    image.src = "https://randomfox.ca/images/2.jpg"
-
-    return image; // retorno el nodo
+    image.width = "320";
+    image.src = "https://randomfox.ca/images/2.jpg";
+    
+    return image; // retorno el nodo de la image
 }
 const newImage = createImageNode();
-document.querySelector("#images").appendChild(newImage);
+const mountNode = document.querySelector("#images");
+mountNode.appendChild(newImage) // la agrego
