af6bd17ae0ba0eaddbaf30377a2b83ae4bf34b87
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2022-01-13 11:28:10 -0400

    summary comments

--- src/index.js
+++ src/index.js
@@ -2,21 +2,14 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
 **/
-/**
- * 1. crear (1) imagen
- * 2. agregar #imagen
- * * 2.1 imagenes aleatorias
- * 3. Evento - DOM 0 -> Agregar imagenes
- * 
- * 4. Intersection observer
- * 5. Lazy loading
-*/
  
-import {  registerImage  } from "./lazy"
+import {  registerImage  } from "./lazy";
 
-const maximum = 122;
-const minimum = 1;
-const random = () => Math.floor(Math.random() * (maximum - minimum) + minimum);
+const random = () => {
+    const maximum = 122;
+    const minimum = 1;
+    return Math.floor(Math.random() * (maximum - minimum) + minimum)
+};
 
 const createImageNode= () => {
     // create container div
@@ -25,18 +18,10 @@ const createImageNode= () => {
 
     // create img
     const image = document.createElement("img");
-    image.className = "mx-auto";
+    image.className = "mx-auto rounded-3xl";
     image.width = "320";
-    // Al momento de cargar la src se carga la imagen por lo que la quitamos
-    // Una forma de hacerla lazy es ponerla en la funcion action de lazy.js
-    // image.src = `https://randomfox.ca/images/${random()}.jpg`;
-
-    // la otra opcion es hacer un dataset que es del DOM
-    // Esta propiedad por si sola contiene la lista de todos los atributos personalizados que le pusiste a tu elemento
     image.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
-
-    // put img inside the container
-    container.appendChild(image);
+    container.appendChild(image); // put img inside the container
 
     return container; // return => node container
 }
@@ -46,7 +31,7 @@ const mountNode = document.querySelector("#images");
 
 const addButton = document.querySelector("button");
 const addImage = () => {
-    const newImage = createImageNode(); // nodo img
+    const newImage = createImageNode(); // node img created
     mountNode.appendChild(newImage) // put into mountNode
     registerImage(newImage); // listen
 }
--- src/lazy.js
+++ src/lazy.js
@@ -1,40 +1,25 @@
-
-// Me dice si hay una interseccion
+// Is intersecting ?
 const isIntersecting = entry => {
-    // isIntersection es una propiedad que retorna un boleano
         // true -> dentro de la pantalla
     return entry.isIntersecting
 }
 
 const loadImage = entry => {
-    // obtener el nodo (div), ya que es lo qeu estoy devolviendo en la funcion createImageNode
-    const container = entry.target; 
-
-    // para obtener la imaen del container le pido que me de el atributo img, ya que es un nodo, pero como la imagen es la unica que hay pudo aplicar un fistChild. Esto para evitar tener que buscar
-    // const image = container.querySelector("img");
-    const image = container.firstChild
+    // put the src to render img
+    const container = entry.target;
+    const image = container.firstChild 
     const url = image.dataset.src;
-
-    // load img
     image.src = url;
- 
-    // image.src = `https://randomfox.ca/images/${random()}.jpg`;
 
-    // des resgistra la imagen (unlisten)
-    observer.unobserve(container)
+    observer.unobserve(container) // unobserve
 }
 
-// Recibe una accion o funcion donde vamos a especificar que hacer por imagen
-// Me va a paser TODAS LAS ENTRADAS que en ese momento esta escuchando
 const observer = new IntersectionObserver( entries => {
     entries
         .filter(isIntersecting)
         .forEach(loadImage)
 })
 
-/**
- * 1. Crear una funcion que resiva una imagen y que me la observe
- */
 export const registerImage = image => {
     // Intersection Observe -> observe(image)
     observer.observe(image);
