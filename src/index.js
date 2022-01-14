e5ffac8d26e62b115547811dbd54b9e130b57d68
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2022-01-14 10:56:36 -0400

    not observe img node

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 /**
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
+ * Objetivos:
+ * 1. Lisener Image Wrapper
+ * 2. Unlisen Imgae Wrapper
+ * 4. Img -> Unobserve
+ * 3. ImgWr obs 50% -> img src create
 **/
  
 import {  registerImage  } from "./lazy";
@@ -40,6 +45,7 @@ const addButton = document.querySelector("button");
 const addImage = () => {
     const newImage = createImageNode(); // node container img created
     mountNode.appendChild(newImage) // put into mountNode
-    registerImage(newImage); // listen the container img
+    // ----
+    // registerImage(newImage); // listen the container img
 }
 addButton.addEventListener("click", addImage);
